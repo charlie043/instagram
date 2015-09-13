48495386c29ad8e1446d9c211c4bc2b6ad1f5727
charlie <h.uematsu1015@gmail.com>
2015-09-13 22:35:03 +0900

    test

--- twitter.js
+++ twitter.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 var config = require('./config');
 var Twitter = require('twitter');
 
-var client = new Twitter(config.twitter);
+var client = new Twitter(config.twitter_dempa);
 
 //client.get('statuses/user_timeline', {
 //  screen_name: 'charlie043'
@@ -17,32 +17,27 @@ var client = new Twitter(config.twitter);
 //});
 
 var text = [
-  '好きだ',
-  '好きだ！',
-  '好きだ!',
-  '好きだッ',
-  '好きだｯ',
-  'スキだ',
-  'スキだ！',
-  'スキだｯ',
-  '好きだ、',
-  'スキダ',
-  'スキなんだ！'
 ];
 
-client.stream('user', {
-}, function(stream) {
-  stream.on('data', function(tweet) {
-    if (tweet.user && tweet.user.screen_name === 'mogatanpe') {
-      client.post('statuses/update', {
-        status: _.sample(text)
-      });
-    }
-  });
-  stream.on('error', function(err) {
-    console.log(err);
-  });
-});
+client.post('statuses/update', {
+  status: 'aaa'
+})
+
+//client.stream('user', {
+//}, function(stream) {
+//  stream.on('data', function(tweet) {
+//    if (tweet.user && tweet.user.screen_name === 'mogatanpe') {
+//      client.post('statuses/update', {
+//        status: _.sample(text)
+//      });
+//    }
+//  });
+//
+//
+//  stream.on('error', function(err) {
+//    console.log(err);
+//  });
+//});
 
 //client.post('statuses/update', {
 //  status: 'ああああああああああああ'
